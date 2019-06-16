58fc553972916d87637d9390cde3bb8111d06e08
kingyy <kingyy2010@gmail.com>
2019-06-16 03:04:47 -0700

    Changed list widget to tree widget.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,6 +8,7 @@ MainWindow::MainWindow(QWidget *parent) :
   ui(new Ui::MainWindow),
   autobot_edit_window_(new AutobotEditWindow(this)){
   ui->setupUi(this);
+  ui->treeWidget_accounts->setColumnWidth(0, 300);
   connect(&bot_log_dialog_, SIGNAL(AddNewAccount(const QString&,
                                                 const QString&)),
           this, SLOT(AddAccount(const QString&, const QString&)));
@@ -36,59 +37,65 @@ void MainWindow::AddAccount(const QString& account_username,
       std::shared_ptr<AutobotAccount> temp_account_ptr
           = std::make_shared<AutobotAccount>(account_username,
                                              account_password);
-      ui->listWidget_accounts->addItem(account_username);
+      QTreeWidgetItem *autobot_item
+          = new QTreeWidgetItem(ui->treeWidget_accounts);
+      ui->treeWidget_accounts->addTopLevelItem(autobot_item);
+      autobot_item->setText(0, account_username);
+      autobot_item->setText(1, "空闲");
       account_manager_.AddAccount(temp_account_ptr);
     }
   }
 }
 
 void MainWindow::on_pushButton_delete_clicked() {
-  QList<QListWidgetItem*> selected_items
-      = ui->listWidget_accounts->selectedItems();
+  QList<QTreeWidgetItem*> selected_items
+      = ui->treeWidget_accounts->selectedItems();
   if (selected_items.empty() == false) {
     QString account_list_msg;
-    foreach(QListWidgetItem * item, selected_items)  {
-      account_list_msg.append(item->text());
+    foreach(QTreeWidgetItem * item, selected_items)  {
+      account_list_msg.append(item->text(0));
       account_list_msg.append(" ");
     }
     QMessageBox messagebox(this);
     messagebox.setWindowTitle("");
     messagebox.setText("您确定删除： " + account_list_msg + "?");
-
     messagebox.addButton("确定", QMessageBox::ButtonRole::AcceptRole);
     messagebox.addButton("取消", QMessageBox::ButtonRole::RejectRole);
     // Not so sure why Accept role coresponds to 0, but reject corepsonds to 1.
     if (messagebox.exec() == false) {
-      foreach(QListWidgetItem * item, selected_items)  {
-        account_manager_.RemoveAutobot(item->text());
-        delete ui->listWidget_accounts->
-            takeItem(ui->listWidget_accounts->row(item));
+      foreach(QTreeWidgetItem * item, selected_items)  {
+        account_manager_.RemoveAutobot(item->text(0));
+        delete item;
       }
     }
   }
 }
-void MainWindow::on_listWidget_accounts_itemClicked(QListWidgetItem *item) {
-  if (autobot_edit_window_->isVisible()) {
-    on_listWidget_accounts_itemDoubleClicked(item);
-  }
-}
 
-void MainWindow::on_listWidget_accounts_itemDoubleClicked(
-    QListWidgetItem *item) {
-  QString account_username = item->text();
+void MainWindow::on_treeWidget_accounts_itemDoubleClicked(
+    QTreeWidgetItem *item, int) {
+  QString account_username = item->text(0);
   std::shared_ptr <AutobotAccount> bot_account_ptr
       = account_manager_.Find(account_username);
   if (bot_account_ptr == nullptr) {
     qFatal(("account name " + account_username + " is not in the manager")
            .toStdString().data());
   } else {
-
     autobot_edit_window_->CombineAutobotAccount(bot_account_ptr);
+    autobot_edit_window_->move(this->pos().x() + this->width(), this->pos().y());
     autobot_edit_window_->show();
   }
 }
 
+void MainWindow::on_treeWidget_accounts_itemClicked(QTreeWidgetItem *item,
+                                                    int column) {
+  if (autobot_edit_window_->isVisible()) {
+    on_treeWidget_accounts_itemDoubleClicked(item, column);
+  }
+}
+
 } //namespace
+
+
 
 
 
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QMessageBox>
 #include <QListWidgetItem>
+#include <QTreeWidgetItem>
 
 #include "autobot_login_dialog.h"
 #include "account_manager.h"
@@ -26,10 +27,9 @@ private slots:
   void AddAccount(const QString&, const QString&);
   void on_pushButton_add_clicked();
   void on_pushButton_delete_clicked();
-
-  void on_listWidget_accounts_itemDoubleClicked(QListWidgetItem *item);
-
-  void on_listWidget_accounts_itemClicked(QListWidgetItem *item);
+  void on_treeWidget_accounts_itemDoubleClicked(QTreeWidgetItem *item,
+                                                int column);
+  void on_treeWidget_accounts_itemClicked(QTreeWidgetItem *item, int column);
 
 private:
     Ui::MainWindow *ui;
